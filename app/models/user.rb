a001ad7a1ed29ceccae5ff2038e405ea69a7b853
WataruOjima <wataru.ojima20@gmail.com>
2020-12-23 16:41:17 +0900

    validationの設定
    パスワードは、半角英数字混合での入力が必須であること
    ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須であること
    ユーザー本名のフリガナは、全角（カタカナ）での入力が必須

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください' 
+
 validates :nickname, presence: true
-validates :last_name, presence: true
-validates :furigana_last_name, presence: true
-validates :first_name, presence: true
-validates :furigana_first_name, presence: true
+with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+end 
+
+with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' } do
+  validates :furigana_last_name, presence: true
+  validates :furigana_first_name, presence: true
+end 
+
 validates :birth_date, presence: true
 
 has_many :items
