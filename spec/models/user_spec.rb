3d362e5ddcee639d1741ac250ab1f16c0f67d8cd
WataruOjima <wataru.ojima20@gmail.com>
2020-12-24 21:07:23 +0900

    追加テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,37 @@ describe User do
     @user = FactoryBot.build(:user)
   end
   describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+      it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+      it "passwordが6文字以上であれば登録できる" do
+        @user.encrypted_password = "abcd123"
+        @user.password_confirmation = "abcd123"
+        expect(@user).to be_valid
+      end
+      it "パスワードに半角英数字混合での入力してあれば登録できる" do
+        @user.encrypted_password = "abcd123"
+        @user.password_confirmation = "abcd123"
+        expect(@user).to be_valid
+      end
+      it "名字が全角文字で入力してあれば登録できる" do
+        @user.last_name = "山田"
+        expect(@user).to be_valid
+      end
+      it "名前が全角文字で入力してあれば登録できる" do
+        @user.first_name = "太郎"
+        expect(@user).to be_valid
+      end
+      it "名字のふりがなが全角カナで入力してあれば登録できる" do 
+        @user.furigana_last_name = "ヤマダ"
+        expect(@user).to be_valid
+      end
+      it "名前のふりがなが全角カナで入力してあれば登録できる" do
+        @user.furigana_first_name = "タロウ"
+        expect(@user).to be_valid
+      end
+    end
     context '新規登録がうまくいかないとき' do
       it "nicknameが空だと登録できない" do
         @user.nickname = ""
@@ -40,7 +71,7 @@ describe User do
       it "passwordに半角英数字混合でなければ登録できない" do
         @user.password = "aaaaaa"
         @user.valid?
-        expect(@user.errors.full_messages).to include("Encrypted password 英字と数字の両方を含めて設定してください")
+        expect(@user.errors.full_messages).to include("Password 英字と数字の両方を含めて設定してください")
       end
       it "passwordが存在してもpassword_confirmationが空では登録できない" do
         @user.password 
