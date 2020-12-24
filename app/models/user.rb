4905666abadefefe93a2f14fa79c9e8452a82037
WataruOjima <wataru.ojima20@gmail.com>
2020-12-24 23:14:48 +0900

    手直しvalidatioon関係

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,22 +5,24 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください' 
+validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'
 
-validates :nickname, presence: true
+
 with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-  validates :last_name, presence: true
-  validates :first_name, presence: true
+  validates :last_name
+  validates :first_name
 end 
 
 with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' } do
-  validates :furigana_last_name, presence: true
-  validates :furigana_first_name, presence: true
+  validates :furigana_last_name
+  validates :furigana_first_name
 end 
 
-validates :birth_date, presence: true
+with_options presence: true do
+  validates :nickname
+  validates :birth_date
+end
 
 has_many :items
 has_many :Purchase_records
-  
 end
