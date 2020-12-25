3eea53335d115d62ec900cbfae38e9ed1385835c
WataruOjima <wataru.ojima20@gmail.com>
2020-12-25 12:18:40 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,10 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :edit, :update, :new, :update, :destroy]
   def index
   end
+  private
 
+  def message_params
+    params.require(:item).permit(:item_name, :user_id, :description, :category_id, :condition_id,
+    :price, :prefecture_id, :shipping_date_id,:shipping_charge_id,:image).merge(user_id: current_user.id)
+  end
 end
