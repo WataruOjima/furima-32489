04a93f71d909f2edd5db4c0f1c56d1ec3de49310
WataruOjima <wataru.ojima20@gmail.com>
2020-12-25 20:52:44 +0900

    ルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,16 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :edit, :update, :new, :update, :destroy]
   def index
+    @items = Item.all
   end
-  private
 
+  def new
+  end
+
+  def create
+  end 
+
+  private
   def message_params
     params.require(:item).permit(:item_name, :user_id, :description, :category_id, :condition_id,
     :price, :prefecture_id, :shipping_date_id,:shipping_charge_id,:image).merge(user_id: current_user.id)
