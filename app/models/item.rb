5c9c29da42ceb885feb0a5428dd9d690ba26c991
WataruOjima <wataru.ojima20@gmail.com>
2020-12-25 20:51:57 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   validates :item_name,          presence: true
   validates :user_id,            presence: true
   validates :description,        presence: true
@@ -10,8 +11,13 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, presence: true
   validates :image,              presence: true
 
+  validates :genre_id, :category_id, :ondition_id, :prefecture_id, :shipping_date_id,
+  numericality: { other_than: 1 } 
 
-has one :Purchase_record
-belongs_to :user
+
+has_one :Purchase_record
 has_one_attached :image
+
+belongs_to  :user, :category_id, :condition_id, :prefecture_id, :shipping_date_id, :shipping_charge_id
+
 end
