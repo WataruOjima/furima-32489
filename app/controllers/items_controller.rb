8bced19872cfe7252fa9fa331ad4716139d889fc
WataruOjima <wataru.ojima20@gmail.com>
2020-12-26 18:37:01 +0900

    ルーティングとコントローラーの変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,14 +5,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
-  end 
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
 
   private
-  def message_params
+  def item_params
     params.require(:item).permit(:item_name, :user_id, :description, :category_id, :condition_id,
     :price, :prefecture_id, :shipping_date_id,:shipping_charge_id,:image).merge(user_id: current_user.id)
   end
 end
+
