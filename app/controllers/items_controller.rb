87a0c0ef6e588fd2eff64087593daf9df7a54180
WataruOjima <wataru.ojima20@gmail.com>
2020-12-29 21:08:36 +0900

    商品編集追加３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :edit, :update, :new, :destroy]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, except: [:index, :show :update]
+  before_action :move_to_index, except: [:index, :show, :update]
   def index
     @items = Item.order('created_at DESC')
   end
