fd80b611961789a85c3eae332f77779dde51c7e9
WataruOjima <wataru.ojima20@gmail.com>
2020-12-30 12:05:31 +0900

    削除機能追加３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :edit, :update, :new, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show, :new, :create, :update]
+  before_action :move_to_index, only: :destroy
   def index
     @items = Item.order('created_at DESC')
   end
